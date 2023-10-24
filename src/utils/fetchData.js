b3ddc62237d1f73b17e4ebd582774bc3c5125bc9
abdourifki <abdourifki1995@gmail.com>
2023-10-24 14:14:44 +0100

    restapi

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key':'bd18344955mshe9fe8c926930e89p18a9d7jsnea52d5b62f00',
+    },
+  };
+  
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': 'bd18344955mshe9fe8c926930e89p18a9d7jsnea52d5b62f00',
+    },
+  };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
