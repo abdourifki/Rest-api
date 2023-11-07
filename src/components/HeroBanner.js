c9bde2182dd5df6f8a3fdd41cdc001e1d6ccd327
abdourifki <abdourifki1995@gmail.com>
2023-11-07 11:16:43 +0100

    dernier-commit

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-
+import HeroBannerImage from '../assets/images/banner.jpg';
 
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
     <Typography color='orange' fontWeight="600" fontSize="26px"   marginBottom="-120px" ></Typography>
-    <Typography fontWeight={700} sx={{ fontSize: { lg: '50px', xs: '40px' } }} mb="23px" mt="30px" style={{color: 'goldenrod'}}>
+    <Typography fontWeight={700} sx={{ fontSize: { lg: '50px', xs: '40px' } }} mb="23px" mt="30px" style={{color: 'orangered'}}>
       " Train, Eat, Sleep, <br />
       And Repeat "
     </Typography>
@@ -15,10 +15,10 @@ const HeroBanner = () => (
     <Stack>
       <a href="#exercises" style={{ fontFamily:'Poppins',marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: 'orangered', padding: '14px', fontSize: '19px', textTransform: 'none', color: 'white', borderRadius: '4px', }}>Explore Exercises</a>
     </Stack>,
-    <Typography fontWeight={600} color="goldenrod" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '180px', color: 'goldenrod', fontFamily:'Poppins', marginTop:'120px', textAlign:'left' }}>
+    <Typography fontWeight={600} color="goldenrod" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '180px', color: 'black', fontFamily:'Poppins', marginTop:'120px', textAlign:'left' }}>
       Exercise
     </Typography>
-    
+    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
   </Box>
 );
 
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,8 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key':'bd18344955mshe9fe8c926930e89p18a9d7jsnea52d5b62f00',
+      'X-RapidAPI-Key':'be24506067msh4a13126b5812c38p1327c7jsn8176c2bbfa4b',
+      
     },
   };
   
